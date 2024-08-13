5f8cfb96c191bbf80adaa38ee37f0934fe19ca08
Martin Schmidt <martin@scoding.dk>
2024-08-13 12:31:36 +0000

    removed default automountServiceAccountToken

--- chart/run_kind_test.sh
+++ chart/run_kind_test.sh
@@ -5,6 +5,7 @@
 
 # Define kind cluster name
 cluster_name=chronicler-test
+namespace=chronicler
 
 # Ensures script fails if something goes wrong.
 set -eo pipefail
@@ -12,24 +13,24 @@ set -eo pipefail
 # define cleanup function
 cleanup() {
     rm -fr $temp_folderx
-    kind delete cluster -n ${cluster_name} >/dev/null 2>&1
+    kind delete cluster --name ${cluster_name} >/dev/null 2>&1
 }
 
 # define debug function
 debug() {
     echo -e "\nDebugging information:"
     echo -e "\nHelm status:"
-    helm status chronicler -n chronicler --show-desc --show-resources
+    helm status chronicler --namespace ${namespace} --show-desc --show-resources
 
     echo -e "\nDeployment description:"
-    kubectl describe deployment -n chronicler po-chronicler-deployment
+    kubectl describe deployment --namespace ${namespace} po-chronicler-deployment
 
-    POD_NAMES=$(kubectl get pods -n chronicler -l app=po-chronicler -o jsonpath="{.items[*].metadata.name}")
+    POD_NAMES=$(kubectl get pods --namespace ${namespace} -l app=po-chronicler -o jsonpath="{.items[*].metadata.name}")
     # Loop over the pods and print their logs
     for POD_NAME in $POD_NAMES
     do
         echo -e "\nLogs for $POD_NAME:"
-        kubectl logs -n chronicler $POD_NAME
+        kubectl logs --namespace ${namespace} $POD_NAME
     done
 }
 
@@ -43,26 +44,26 @@ values_filename=${temp_folder}/values.yaml
 secret_filename=${temp_folder}/secret.yaml
 
 # create kind cluster
-kind delete cluster -n ${cluster_name}
-kind create cluster -n ${cluster_name}
+kind delete cluster --name ${cluster_name}
+kind create cluster --name ${cluster_name}
 
 # create namespace
-kubectl create namespace chronicler
+kubectl create namespace ${namespace}
 
 # install postgresql chart
-helm install postgresql oci://registry-1.docker.io/bitnamicharts/postgresql --namespace chronicler
+helm install postgresql oci://registry-1.docker.io/bitnamicharts/postgresql --namespace ${namespace}
 
 # build docker image
 docker build -f src/Chronicler.Dockerfile -t ghcr.io/project-origin/chronicler:test src/
 
 # load docker image into cluster
-kind load -n ${cluster_name} docker-image ghcr.io/project-origin/chronicler:test
+kind load --name ${cluster_name} docker-image ghcr.io/project-origin/chronicler:test
 
 # generate keys
 openssl genpkey -algorithm ED25519 > ${secret_filename}
 
 # generate secret
-kubectl create secret generic signing-key --from-file=my-key=${secret_filename} --namespace chronicler
+kubectl create secret generic signing-key --from-file=my-key=${secret_filename} --namespace ${namespace}
 
 # generate values.yaml file
 cat << EOF > "${values_filename}"
@@ -78,7 +79,7 @@ config:
   networkConfigurationFile: |-
     {
       "RegistryUrls": {
-        "narniaRegistry": "https://registry.narnia.example.com",
+        "narniaRegistry": "https://registry.narnia.example.com"
       }
     }
 postgresql:
@@ -93,6 +94,15 @@ postgresql:
 EOF
 
 # install chronicler chart
-helm install chronicler ./chart --values ${values_filename} --namespace chronicler --wait
+helm install chronicler ./chart --values ${values_filename} --namespace ${namespace} --wait --timeout 1m
 
-echo "Test completed successfully ✅"
+# verify deployment is ready
+deployments_status=$(kubectl get deployments --namespace ${namespace} --no-headers | awk '$3 != $4 {print "Deployment " $1 " is not ready"}')
+
+# Print the results to stderr if there are any issues
+if [ -n "$deployments_status" ]; then
+    echo "$deployments_status" 1>&2
+    echo "Test failed ❌"
+else
+    echo "Test completed successfully ✅"
+fi
